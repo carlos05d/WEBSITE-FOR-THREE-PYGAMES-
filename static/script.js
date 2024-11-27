546eaff1c30bd27b8816c5848e062712cb729c04
mustafa <hassan.1990.kharacha@gmail.com>
2024-11-27 17:34:20 +0100

    secend tetri

--- static/script.js
+++ static/script.js
@@ -2,9 +2,17 @@ const themeToggleButton = document.getElementById("theme-toggle");
 const body = document.body;
 
 // Check local storage for theme preference
-if (localStorage.getItem("theme") === "dark") {
+const savedTheme = localStorage.getItem("theme");
+if (savedTheme === "dark") {
     body.classList.add("dark-mode");
     themeToggleButton.textContent = "White Mode";
+} else if (savedTheme === "light") {
+    body.classList.remove("dark-mode");
+    themeToggleButton.textContent = "Night Mode";
+} else {
+    // Default to light theme if no preference is saved
+    localStorage.setItem("theme", "light");
+    themeToggleButton.textContent = "Night Mode";
 }
 
 // Toggle between light and dark mode
